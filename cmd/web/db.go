1de601203f1702edb21f538b2f38b4a46c245c89
rbalajee <rahrkb4@gmail.com>
2025-07-22 16:47:20 +0530

    connect to db

---
+++ cmd/web/db.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"context"
+	"database/sql"
+	"time"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+const (
+	maxOpenDbConn = 25
+	maxIdleDbConn = 25
+	maxDbLifetime = 5 * time.Minute
+)
+
+func initMySQLDB(dsn string) (*sql.DB, error) {
+	db, err := sql.Open("mysql", dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+	defer cancel()
+
+	// test connection
+	if err = db.PingContext(ctx); err != nil {
+		return nil, err
+	}
+
+	db.SetMaxOpenConns(maxOpenDbConn)
+	db.SetMaxIdleConns(maxIdleDbConn)
+	db.SetConnMaxLifetime(maxDbLifetime)
+
+	return db, nil
+}
--- go.mod
+++ go.mod
@@ -2,9 +2,9 @@ module github.com/rahulbalajee/design-patterns
 
 go 1.24.1
 
-require github.com/go-chi/chi/v5 v5.2.2
-
 require (
-	filippo.io/edwards25519 v1.1.0 // indirect
-	github.com/go-sql-driver/mysql v1.9.3 // indirect
+	github.com/go-chi/chi/v5 v5.2.2
+	github.com/go-sql-driver/mysql v1.9.3
 )
+
+require filippo.io/edwards25519 v1.1.0 // indirect
