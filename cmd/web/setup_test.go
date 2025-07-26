9dcd27d83cbaef724eef997482fe0af1b443cf94
rahulbalajee <rahrkb4@gmail.com>
2025-07-26 18:28:04 +0530

    added test

--- cmd/web/setup_test.go
+++ cmd/web/setup_test.go
@@ -5,15 +5,34 @@ import (
 	"testing"
 
 	"github.com/rahulbalajee/design-patterns/configuration"
+	"github.com/rahulbalajee/design-patterns/models"
 )
 
 var testApp application
 
 func TestMain(m *testing.M) {
 
+	testBackend := &TestBackend{}
+	testAdapter := &RemoteService{Remote: testBackend}
+
 	testApp = application{
-		App: configuration.New(nil),
+		App:        configuration.New(nil),
+		catService: testAdapter,
 	}
 
 	os.Exit(m.Run())
+}
+
+type TestBackend struct{}
+
+func (tb *TestBackend) GetAllCatBreeds() ([]*models.CatBreed, error) {
+	breeds := []*models.CatBreed{
+		&models.CatBreed{
+			ID:      1,
+			Breed:   "Tomcat",
+			Details: "something",
+		},
+	}
+
+	return breeds, nil
 }
